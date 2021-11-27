fca1012df852e18a76e544e8e48e1697a6d10311
Minwoo Park <32972772+minwoopark2003@users.noreply.github.com>
2021-11-26 22:31:35 -0800

    Add files via upload

---
+++ FinalProjectCode.ino
@@ -0,0 +1,444 @@
+// use the IRremote library to use IR receiver in code
+#include <IRremote.h>
+// use the LCD library to use LCD in the code
+#include <LiquidCrystal.h>
+
+// constants for all note's frequencies
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST 0
+
+// Passive buzzer pin on Arduino
+const int buzzerPin = 8;
+// IR receiver pin on Arduino
+const int RECV_PIN = 7;
+// PIR motion sensor output pin on Arduino
+const int PIR_PIN = 19;
+
+
+// Set the variables that store the values of the buttons on the remote control 
+const unsigned long button1 = 0xF30CFF00;
+const unsigned long button2 = 0xE718FF00;
+const unsigned long button3 = 0xA15EFF00;
+
+// constant variables holding the LCD pins 
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+
+// creating an  LCD object 
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Notes for super mario theme song
+int marioThemeMelody[] = {NOTE_E7, NOTE_E7, 0, NOTE_E7,
+  0, NOTE_C7, NOTE_E7, 0,
+  NOTE_G7, 0, 0,  0,
+  NOTE_G6, 0, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0,
+
+  NOTE_C7, 0, 0, NOTE_G6,
+  0, 0, NOTE_E6, 0,
+  0, NOTE_A6, 0, NOTE_B6,
+  0, NOTE_AS6, NOTE_A6, 0,
+
+  NOTE_G6, NOTE_E7, NOTE_G7,
+  NOTE_A7, 0, NOTE_F7, NOTE_G7,
+  0, NOTE_E7, 0, NOTE_C7,
+  NOTE_D7, NOTE_B6, 0, 0};
+
+// Notes for star wars theme song
+int starWarsThemeMelody[] = {NOTE_AS4, NOTE_AS4, NOTE_AS4,//1
+  NOTE_F5, NOTE_C6,
+  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,  
+  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,  
+  NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5, NOTE_C5,
+  NOTE_F5, NOTE_C6,
+  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6,  
+  
+  NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F6, NOTE_C6, //8  
+  NOTE_AS5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_C5, NOTE_C5, 
+  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+  NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5,NOTE_C5, NOTE_C5,
+  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+  
+  NOTE_C6, NOTE_G5, NOTE_G5, 0, NOTE_C5,//13
+  NOTE_D5, NOTE_D5, NOTE_AS5, NOTE_A5, NOTE_G5, NOTE_F5,
+  NOTE_F5, NOTE_G5, NOTE_A5, NOTE_G5, NOTE_D5, NOTE_E5,NOTE_C6, NOTE_C6,
+  NOTE_F6, NOTE_DS6, NOTE_CS6, NOTE_C6, NOTE_AS5, NOTE_GS5, NOTE_G5, NOTE_F5,
+  NOTE_C6};
+
+// Note types for super mario theme song
+int marioThemeDuration[] = {12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+
+  9, 9, 9,
+  12, 12, 12, 12,
+  12, 12, 12, 12,
+  12, 12, 12, 12,};
+// Note types for Star wars theme song
+int starWarsThemeDuration[] = {8, 8, 8, 2, 2, 8, 8, 8, 2, 4, 
+8, 8, 8, 2, 4, 8, 8, 8, 2, 8, 8, 8, 2, 2, 8, 8, 8, 2, 4, 8, 8, 8, 
+2, 4, 8, 8, 8, 2, -8, 16, -4, 8, 8, 8, 8, 8, 8, 8, 8, 4, 8, 4, -8, 16, -4,
+8, 8, 8, 8, 8, -8, 16, 2, 8, 8, -4, 8, 8, 8, 8, 8, 8, 8, 8, 4, 8, 4, -8, 16,
+4, 8, 4, 8, 4, 8, 4, 8, 1};
+
+
+// Tetris theme song 
+int tetrisThemeSong[] = {
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, 0, 4,
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,8,  NOTE_A4,4,  NOTE_B4,8,  NOTE_C5,8,
+
+  NOTE_D5, -4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, 0, 4,
+  
+
+  NOTE_E5,2,  NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,2,   NOTE_A4,2,
+  NOTE_GS4,2,  NOTE_B4,4,  0,8, 
+  NOTE_E5,2,   NOTE_C5,2,
+  NOTE_D5,2,   NOTE_B4,2,
+  NOTE_C5,4,   NOTE_E5,4,  NOTE_A5,2,
+  NOTE_GS5,2,};
+
+// Pink Panther theme song
+int pinkPantherThemeSong[] = {REST,2, REST,4, REST,8, NOTE_DS4,8, 
+  NOTE_E4,-4, REST,8, NOTE_FS4,8, NOTE_G4,-4, REST,8, NOTE_DS4,8,
+  NOTE_E4,-8, NOTE_FS4,8,  NOTE_G4,-8, NOTE_C5,8, NOTE_B4,-8, NOTE_E4,8, NOTE_G4,-8, NOTE_B4,8,   
+  NOTE_AS4,2, NOTE_A4,-16, NOTE_G4,-16, NOTE_E4,-16, NOTE_D4,-16, 
+  NOTE_E4,2, REST,4, REST,8, NOTE_DS4,4,
+
+  NOTE_E4,-4, REST,8, NOTE_FS4,8, NOTE_G4,-4, REST,8, NOTE_DS4,8,
+  NOTE_E4,-8, NOTE_FS4,8,  NOTE_G4,-8, NOTE_C5,8, NOTE_B4,-8, NOTE_G4,8, NOTE_B4,-8, NOTE_E5,8,
+  NOTE_DS5,1,   
+  NOTE_D5,2, REST,4, REST,8, NOTE_DS4,8, 
+  NOTE_E4,-4, REST,8, NOTE_FS4,8, NOTE_G4,-4, REST,8, NOTE_DS4,8,
+  NOTE_E4,-8, NOTE_FS4,8,  NOTE_G4,-8, NOTE_C5,8, NOTE_B4,-8, NOTE_E4,8, NOTE_G4,-8, NOTE_B4,8,   
+  
+  NOTE_AS4,2, NOTE_A4,-16, NOTE_G4,-16, NOTE_E4,-16, NOTE_D4,-16, 
+  NOTE_E4,-4, REST,4,
+  REST,4, NOTE_E5,-8, NOTE_D5,8, NOTE_B4,-8, NOTE_A4,8, NOTE_G4,-8, NOTE_E4,-8,
+  NOTE_AS4,16, NOTE_A4,-8, NOTE_AS4,16, NOTE_A4,-8, NOTE_AS4,16, NOTE_A4,-8, NOTE_AS4,16, NOTE_A4,-8,   
+  NOTE_G4,-16, NOTE_E4,-16, NOTE_D4,-16, NOTE_E4,16, NOTE_E4,16, NOTE_E4,2,};
+
+// Song for stand-by mode
+int waitingThemeSong[] = {NOTE_FS4,8, REST,8, NOTE_A4,8, NOTE_CS5,8, REST,8,NOTE_A4,8, REST,8, NOTE_FS4,8, //1
+  NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, REST,8, REST,4, REST,8, NOTE_CS4,8,
+  NOTE_D4,8, NOTE_FS4,8, NOTE_A4,8, NOTE_CS5,8, REST,8, NOTE_A4,8, REST,8, NOTE_F4,8,
+  NOTE_E5,-4, NOTE_DS5,8, NOTE_D5,8, REST,8, REST,4,
+  
+  NOTE_GS4,8, REST,8, NOTE_CS5,8, NOTE_FS4,8, REST,8,NOTE_CS5,8, REST,8, NOTE_GS4,8, //5
+  REST,8, NOTE_CS5,8, NOTE_G4,8, NOTE_FS4,8, REST,8, NOTE_E4,8, REST,8,
+  NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, REST,8, REST,4, NOTE_E4,8, NOTE_E4,8,
+  NOTE_E4,8, REST,8, REST,4, NOTE_DS4,8, NOTE_D4,8, 
+
+  NOTE_CS4,8, REST,8, NOTE_A4,8, NOTE_CS5,8, REST,8,NOTE_A4,8, REST,8, NOTE_FS4,8, //9
+  NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, REST,8, NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, REST,8,
+  REST,8, NOTE_FS4,8, NOTE_A4,8, NOTE_CS5,8, REST,8, NOTE_A4,8, REST,8, NOTE_F4,8,
+  NOTE_E5,2, NOTE_D5,8, REST,8, REST,4,
+
+  NOTE_B4,8, NOTE_G4,8, NOTE_D4,8, NOTE_CS4,4, NOTE_B4,8, NOTE_G4,8, NOTE_CS4,8, //13
+  NOTE_A4,8, NOTE_FS4,8, NOTE_C4,8, NOTE_B3,4, NOTE_F4,8, NOTE_D4,8, NOTE_B3,8,
+  NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, REST,4, REST,4, NOTE_AS4,4,
+  NOTE_CS5,8, NOTE_D5,8, NOTE_FS5,8, NOTE_A5,8, REST,8, REST,4, 
+
+  REST,2, NOTE_A3,4, NOTE_AS3,4, //17 
+  NOTE_A3,-4, NOTE_A3,8, NOTE_A3,2,
+  REST,4, NOTE_A3,8, NOTE_AS3,8, NOTE_A3,8, NOTE_F4,4, NOTE_C4,8,
+  NOTE_A3,-4, NOTE_A3,8, NOTE_A3,2,
+
+  REST,2, NOTE_B3,4, NOTE_C4,4, //21
+  NOTE_CS4,-4, NOTE_C4,8, NOTE_CS4,2,
+  REST,4, NOTE_CS4,8, NOTE_C4,8, NOTE_CS4,8, NOTE_GS4,4, NOTE_DS4,8,
+  NOTE_CS4,-4, NOTE_DS4,8, NOTE_B3,1,
+  
+  NOTE_E4,4, NOTE_E4,4, NOTE_E4,4, REST,8,//25
+
+  //repeats 1-25
+
+  NOTE_FS4,8, REST,8, NOTE_A4,8, NOTE_CS5,8, REST,8,NOTE_A4,8, REST,8, NOTE_FS4,8, //1
+  NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, REST,8, REST,4, REST,8, NOTE_CS4,8,
+  NOTE_D4,8, NOTE_FS4,8, NOTE_A4,8, NOTE_CS5,8, REST,8, NOTE_A4,8, REST,8, NOTE_F4,8,
+  NOTE_E5,-4, NOTE_DS5,8, NOTE_D5,8, REST,8, REST,4,
+  
+  NOTE_GS4,8, REST,8, NOTE_CS5,8, NOTE_FS4,8, REST,8,NOTE_CS5,8, REST,8, NOTE_GS4,8, //5
+  REST,8, NOTE_CS5,8, NOTE_G4,8, NOTE_FS4,8, REST,8, NOTE_E4,8, REST,8,
+  NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, REST,8, REST,4, NOTE_E4,8, NOTE_E4,8,
+  NOTE_E4,8, REST,8, REST,4, NOTE_DS4,8, NOTE_D4,8, 
+
+  NOTE_CS4,8, REST,8, NOTE_A4,8, NOTE_CS5,8, REST,8,NOTE_A4,8, REST,8, NOTE_FS4,8, //9
+  NOTE_D4,8, NOTE_D4,8, NOTE_D4,8, REST,8, NOTE_E5,8, NOTE_E5,8, NOTE_E5,8, REST,8,
+  REST,8, NOTE_FS4,8, NOTE_A4,8, NOTE_CS5,8, REST,8, NOTE_A4,8, REST,8, NOTE_F4,8,
+  NOTE_E5,2, NOTE_D5,8, REST,8, REST,4,
+
+  NOTE_B4,8, NOTE_G4,8, NOTE_D4,8, NOTE_CS4,4, NOTE_B4,8, NOTE_G4,8, NOTE_CS4,8, //13
+  NOTE_A4,8, NOTE_FS4,8, NOTE_C4,8, NOTE_B3,4, NOTE_F4,8, NOTE_D4,8, NOTE_B3,8,
+  NOTE_E4,8, NOTE_E4,8, NOTE_E4,8, REST,4, REST,4, NOTE_AS4,4,
+  NOTE_CS5,8, NOTE_D5,8, NOTE_FS5,8, NOTE_A5,8, REST,8, REST,4, 
+
+  REST,2, NOTE_A3,4, NOTE_AS3,4, //17 
+  NOTE_A3,-4, NOTE_A3,8, NOTE_A3,2,
+  REST,4, NOTE_A3,8, NOTE_AS3,8, NOTE_A3,8, NOTE_F4,4, NOTE_C4,8,
+  NOTE_A3,-4, NOTE_A3,8, NOTE_A3,2,
+
+  REST,2, NOTE_B3,4, NOTE_C4,4, //21
+  NOTE_CS4,-4, NOTE_C4,8, NOTE_CS4,2,
+  REST,4, NOTE_CS4,8, NOTE_C4,8, NOTE_CS4,8, NOTE_GS4,4, NOTE_DS4,8,
+  NOTE_CS4,-4, NOTE_DS4,8, NOTE_B3,1,
+  
+  NOTE_E4,4, NOTE_E4,4, NOTE_E4,4, REST,8};
+
+
+
+// calculates the duration of a note given note type
+// Ex) quarter note is equal to 4 in the Note Duration array so it last 250 ms
+float calculateDuration(int duration){ 
+  int processedDuration = 0;
+  // this if statement checks whether something is a dotted note, this indicated by negatives in the song arrays
+  if(duration< 0){
+    processedDuration = 1000/abs(duration);
+    // since it is dotted note duration multiplied by 1.5
+    processedDuration *=1.5;
+  } else{
+    // if it isn't a dotted note process normally by dividing by note type
+    processedDuration = 1000/duration;
+  }
+  return processedDuration;
+}
+
+
+// function that will make the buzzer play a specific note for a certain amount of time
+void sing(int buzzerPin, int frequency, int duration){
+  // calculate how long in ms each note is using calculateDuration() 
+  duration = calculateDuration(duration);
+  // play note
+  tone(buzzerPin,frequency,duration);
+  // pause to allow for note to play for full duration
+  delay(duration);
+  // turn off the buzzer to allow for the next note
+  noTone(buzzerPin);
+  // pause to give some delay before next note
+  delay(duration);
+}
+
+// turn on and off LCD display accordingly to motion sensor results
+void turnDisplay(){
+  // turn on LCD
+  lcd.display();
+  // flicker red LED
+  digitalWrite(13, HIGH);
+  delay(250);
+  digitalWrite(13,LOW);
+}
+
+void setup(){
+  // create a serial monitor 
+  Serial.begin(9600);
+  // set pin mode for passive buzzer
+  pinMode(buzzerPin, OUTPUT);
+  // set pin mode for red LED
+  pinMode(13, OUTPUT);
+  // set pin mode for PIR motion sensor 
+  pinMode(PIR_PIN, INPUT_PULLUP);
+  // hardware interrupt, constantly checking if the motion sensor is activated to turn on display
+  attachInterrupt(digitalPinToInterrupt(19), turnDisplay, HIGH);
+  // setting up LCD with 1 rows and 16 columns 
+  lcd.begin(16,1);
+  // Allow IR receiver to take input from remote control
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
+}
+
+// create string variable that will hold name of current song
+String currentSong = "";
+
+// create a boolean variable for while loop testing
+boolean test = false;
+
+void loop(){
+  // This while loop here for demonstration of understanding and usuage of while loop 
+  // It will never actually run because the condition is always false
+  while(test){
+    Serial.println("This will never print");
+  }
+  // if the IR receiver receives data 
+  if (IrReceiver.decode()){
+        // print the hexadecimal code for the data received 
+        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+        
+        // turn on autoscrolling on LCD
+          lcd.autoscroll();
+        // put cursor of lcd at the right end to allow for autoscrolling
+          lcd.setCursor(16,1);
+        // at the beginning don't show anything on the LCD
+          lcd.noDisplay();
+          
+        switch(IrReceiver.decodedIRData.decodedRawData){
+          // when button 1 on remote is pressed
+          case button1:
+          noTone(buzzerPin);
+          // set current song name 
+          currentSong = " Star Wars Theme Song";
+          // play the star wars theme song
+          for(int i= 0;i<sizeof(starWarsThemeMelody)/sizeof(int);i++){
+            // print out each letter of the song's name
+            lcd.print(currentSong[i%currentSong.length()]);
+            // sing each note of the song
+            sing(buzzerPin, starWarsThemeMelody[i],starWarsThemeDuration[i]);
+          }
+          break;
+
+          // when button 2 on remote is pressed
+          case button2:
+          noTone(buzzerPin);
+          // set current song name
+          currentSong = " Tetris Theme Song";
+          // play the tetris theme song
+          for(int j = 0;j<sizeof(tetrisThemeSong)/sizeof(int);j+=2){
+             // print out each letter of the song's name
+             lcd.print(currentSong[(j/2)%currentSong.length()]);
+             // sing each note of the song
+             sing(buzzerPin, tetrisThemeSong[j],tetrisThemeSong[j+1]);
+          }
+          break;
+
+          // when button 3 on remote is pressed
+          case button3:
+          noTone(buzzerPin);
+          // set current song name
+          currentSong = " Pink Panther Theme song";
+          // play the pink panther theme song
+          for(int j = 0;j<sizeof(pinkPantherThemeSong)/sizeof(int);j+=2){
+             lcd.print(currentSong[(j/2)%currentSong.length()]);
+             sing(buzzerPin, pinkPantherThemeSong[j],pinkPantherThemeSong[j+1]);
+          }
+          break;
+        }
+        // search for another IR signal 
+        IrReceiver.resume();
+}
+}
